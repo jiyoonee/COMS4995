66b9b5a51e3a917ad18afe7752e97683e52f1dd2
jiyoonee <js5360@columbia.edu>
2020-11-13 05:36:48 +0900

    moved test files to client folder

---
+++ client/build/precache-manifest.3e841e4ffe2c9f232c8d88ec65209b30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b18fd9a5e891fb21818426e58c7cf6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69ab3235e5e2ca76c5aa",
+    "url": "/static/css/main.55b92666.chunk.css"
+  },
+  {
+    "revision": "fa7172e7c31fc4716352",
+    "url": "/static/js/2.43398ba0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.43398ba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ab3235e5e2ca76c5aa",
+    "url": "/static/js/main.755c1b60.chunk.js"
+  },
+  {
+    "revision": "e03e2071c2560b95e021",
+    "url": "/static/js/runtime-main.217c330b.js"
+  }
+]);
